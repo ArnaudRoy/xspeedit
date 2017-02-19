1264aab947d0785b73b2dc1c0675687af4f5dbbd
Arnaud ROY <arnaud.roy@cgi.com>
2017-02-19 12:03:26 +0100

    Correction nommage EmballageBasiqueService

---
+++ src/main/java/fr/aroy/xspeedit/service/EmballageServiceBasique.java
@@ -0,0 +1,39 @@
+package fr.aroy.xspeedit.service;
+
+import fr.aroy.xspeedit.domain.Article;
+import fr.aroy.xspeedit.domain.Carton;
+import fr.aroy.xspeedit.domain.EspaceDeStockage;
+import fr.aroy.xspeedit.domain.EspaceDeStockageRepository;
+
+public class EmballageServiceBasique implements EmballageService {
+	
+	EspaceDeStockageRepository espaceDeStockageRepository; 
+
+	public EmballageServiceBasique() {
+	}
+	
+	public EmballageServiceBasique(EspaceDeStockageRepository espaceDeStockageRepository) {
+		this.espaceDeStockageRepository = espaceDeStockageRepository;
+	}
+	
+	@Override
+	public void emballer(Article[] chaineDArticles) {
+		EspaceDeStockage espaceDeStockage = espaceDeStockageRepository.getEspaceDeStockage();
+		
+		Carton carton = new Carton();
+		for (Article article : chaineDArticles) {
+			if (carton.)
+		}
+	}
+
+	@Override
+	public Carton[] getCartonsALivrer() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	public void setEspaceDeStockageRepository(EspaceDeStockageRepository espaceDeStockageRepository) {
+		this.espaceDeStockageRepository = espaceDeStockageRepository;
+	}
+
+}
