cb22c211c09d90f6a9428ece88f52f0627e50001
Arnaud ROY <arnaud.roy@cgi.com>
2017-02-19 11:14:15 +0100

    Completion du Test sur l'emballage basique

--- src/test/java/fr/aroy/xspeedit/EmballageServiceTest.java
+++ src/test/java/fr/aroy/xspeedit/EmballageServiceTest.java
@@ -8,6 +8,7 @@ import org.junit.Test;
 
 import fr.aroy.xspeedit.domain.Article;
 import fr.aroy.xspeedit.domain.Carton;
+import fr.aroy.xspeedit.service.EmballageBasique;
 import fr.aroy.xspeedit.service.EmballageService;
 import fr.aroy.xspeedit.service.EmballageUtils;
 
@@ -15,13 +16,17 @@ public class EmballageServiceTest {
 
 	@Test
 	public void test() {
-		EmballageService emballageService = null;
+		EmballageService emballageService = new EmballageBasique();
 		
 		Article[] articles = EmballageUtils.transformChaineDeChiffresEnChaineDArticles("163841689525773");
 		
 		emballageService.emballer(articles);
 		Carton[] chaineDeCartons= emballageService.getCartonsALivrer();
+
 		assertThat(chaineDeCartons, arrayWithSize(equalTo(10)));
+		
+		String chaineDeCartonEnString = EmballageUtils.transformChaineDeCartonsEnChaineDeChiffres(chaineDeCartons);
+		assertThat(chaineDeCartonEnString, equalTo("163/8/41/6/8/9/52/5/7/73"));
 	}
 
 }
