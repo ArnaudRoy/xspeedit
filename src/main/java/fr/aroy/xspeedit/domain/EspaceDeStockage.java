d9b4ac36beeb085d0fc9eae1b8e0e44faf4c33e2
Arnaud ROY <arnaud.roy@cgi.com>
2017-02-19 12:05:44 +0100

    Espace de stockage : addCarton + getChaine

--- src/main/java/fr/aroy/xspeedit/domain/EspaceDeStockage.java
+++ src/main/java/fr/aroy/xspeedit/domain/EspaceDeStockage.java
@@ -1,5 +1,21 @@
 package fr.aroy.xspeedit.domain;
 
+import java.util.Set;
+
 public class EspaceDeStockage {
 
+	public Set<Carton> chaineDeCartons;
+	
+	public EspaceDeStockage() {
+		super();
+	}
+	
+	public void addCarton(Carton carton) {
+		chaineDeCartons.add(carton);
+	}
+	
+	public Set<Carton> getChaineDeCartons() {
+		return chaineDeCartons;
+	}
+	
 }
